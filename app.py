053a5f6fc1fe890096c8e4fc6cf00f4141fd950e
Yassutyy <yaswanth10prime@gmail.com>
2025-06-17 09:36:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ if option == "🏠 Home":
         - Shows dataset used for training the car price predictor
         - Visualizes car price trends
         - Predicts price using:
-            - Linear Regression
-            - Random Forest
+            - Linear Regression R2 Score : 0.310
+            - Random Forest R2 Score : 0.648
         ---
         👉 Use the sidebar to explore!
     """)
